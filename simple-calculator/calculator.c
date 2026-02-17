b7e72665d5b11cea5415b665c91ed2edc2cf0fbf
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-02-17 18:31:16 +0100

    finish project tasks

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 int main(void) {
-	int operation;
-
-	printf("Welcome - Simple Calculator\n");
+	int operation, a, b, result;
 
 	while (operation != 0) {
-		printf("------------------------------\n");
+		printf("\n------------------------------\n");
+		printf("  --- Simple Calculator ---  \n");
 		printf("------------------------------\n");
 		printf("1) Add\n");
 		printf("2) Subtract\n");
@@ -24,8 +23,45 @@ int main(void) {
 
 		if (operation < 0 || operation > 4)
 			printf("Invalid choice\n");
+		else if (operation == 0) {
+			printf("Bye !\n");
+			return (0);
+		}
+		else {
+			
+			printf("Choose the first number (A) for the operation\n");
+			scanf("%d", &a);
+			printf("A : %d\n", a);
+
+			printf("Choose the second number (B) for the operation\n");
+			scanf("%d", &b);
+			printf("B : %d\n", b);
+
+			switch (operation) {
+				case 1:
+					result = a + b;
+					printf("Result : %d\n", result);
+					break;
+				case 2:
+					result = a - b;
+					printf("Result : %d\n", result);
+					break;
+				case 3:
+					result = a * b;
+					printf("Result : %d\n", result);
+					break;
+				case 4:
+					if (b == 0)
+						printf("Error: division by zero\n");
+					else
+					{
+						result = a / b;
+						printf("Result : %d\n", result);
+					}
+					break;
+			}
+		}		
 	}
 
-	printf("Bye !\n");
 	return (0);
 }
